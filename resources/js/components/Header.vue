655f6425e20ccfa089434749662ddef3cd29a70f
deleman <computercodemohammad@gamil.com>
2019-02-10 09:41:48 -0800

    add dependecy by run npm install

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>{{name}}</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+
+            name: 'mohamamd ravand'
+        }
+    }
+}
+</script>
